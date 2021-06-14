14f1f3b225f7452861fd90a73b9e2300e4132a36
neff-1 <47888850+neff-1@users.noreply.github.com>
2021-06-15 00:21:53 +0800

    Update home_page.dart
    Enthalpy(liquid) is working so far, if the input is in temperature(Fahrenheit).

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,15 +8,116 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  var temp, result;
+  static const Tc = 647.096, pc = 22064000, alpha_0 = 1000, rho_c = 322;
+  double dAlpha = -1135.905627715;
+  var result,
+      pres,
+      theta,
+      tau,
+      tempVar,
+      ans,
+      lntau,
+      lnTau0,
+      lnTau1,
+      lnTau2,
+      ans1,
+      temp;
+
+  List<double> a = [
+    -7.85951783,
+    1.84408259,
+    -11.7866497,
+    22.6807411,
+    -15.9618719,
+    1.80122502
+  ];
+
+  List<double> b = [
+    1.99274064,
+    1.09965342,
+    -0.510839303,
+    -1.75493479,
+    -45.5170352,
+    -674694.45
+  ];
+
+  List<double> d = [
+    -0.0000000565134998,
+    2690.66631,
+    127.287297,
+    -135.003439,
+    0.981825814
+  ];
 
   final TextEditingController t1 = new TextEditingController(text: "0");
 
-  void doSolve() {
+  double satPres(double temp) {
+    theta = temp / Tc;
+    tau = 1 - theta;
+    tempVar = (Tc / temp) *
+        (a[0] * tau +
+            a[1] * pow(tau, 1.5) +
+            a[2] * pow(tau, 3) +
+            a[3] * pow(tau, 3.5) +
+            a[4] * pow(tau, 4) +
+            a[5] * pow(tau, 7.5));
+    ans = pc * exp(tempVar);
+    return ans;
+  }
+
+  double rhoL(double temp) {
+    theta = temp / Tc;
+    tau = 1 - theta;
+    lntau = log(tau);
+    tempVar = 1 +
+        b[0] * exp(lntau / 3.0) +
+        b[1] * exp(2.0 * lntau / 3.0) +
+        b[2] * exp(5.0 * lntau / 3.0) +
+        b[3] * exp(16.0 * lntau / 3.0) +
+        b[4] * exp(43.0 * lntau / 3.0) +
+        b[5] * exp(110.0 * lntau / 3.0);
+    ans = rho_c * (tempVar * 1.0);
+    return ans;
+  }
+
+  double alpha(double temp) {
+    theta = temp / Tc;
+    tempVar = dAlpha +
+        d[0] * pow(theta, -19) +
+        d[1] * pow(theta, 1) +
+        d[2] * pow(theta, 4.5) +
+        d[3] * pow(theta, 5) +
+        d[4] * pow(theta, 54.5);
+    ans = alpha_0 * tempVar;
+    return ans;
+  }
+
+  double dpdT(double pres, double temp) {
+    theta = temp / Tc;
+    tau = 1 - theta;
+    lnTau0 = 6.5 * log(tau);
+    lnTau1 = 2.5 * log(tau);
+    lnTau2 = 0.5 * log(tau);
+    tempVar = (7.5 * a[5] * exp(lnTau0)) +
+        (4 * a[4] * pow(tau, 3)) +
+        (3.5 * a[3] * exp(lnTau1)) +
+        (3 * a[2] * pow(tau, 2)) +
+        (1.5 * a[1] * exp(lnTau2)) +
+        a[0] +
+        (log(pres / pc));
+    ans = -1 * (pres / temp) * tempVar;
+    return ans;
+  }
+
+  void tempInputEnthalpy() {
     setState(() {
       temp = double.parse(t1.text);
-      result = ((temp - 32) / 1.8) + 237.15;
-      result = result.toStringAsFixed(2);
+      temp = ((temp - 32) / 1.8) + 273.15;
+      pres = satPres(temp);
+
+      result = alpha(temp) + ((temp / rhoL(temp)) * dpdT(pres, temp));
+      result = result / (1055.056 * 2.2046226);
+      result = result.toStringAsFixed(6);
     });
   }
 // functions here
@@ -30,9 +133,9 @@ class HomePageState extends State<HomePage> {
         child: new ListView(
           children: [
             new Text(
-              "Output: $result",
+              "Enthalpy(liquid) is: $result" + " Btu/lb",
               style: new TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 16.0,
                   fontWeight: FontWeight.bold,
                   color: Colors.purple),
             ),
@@ -49,11 +152,11 @@ class HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 new TextButton(
-                    child: new Text("Clear"),
+                    child: new Text("Solve"),
                     style: ButtonStyle(
                         backgroundColor:
                             MaterialStateProperty.all(Colors.greenAccent)),
-                    onPressed: doSolve),
+                    onPressed: tempInputEnthalpy),
               ],
             )
           ],
